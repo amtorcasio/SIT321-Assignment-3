558bb389350dbc1212d7f9a319fbddc62aab8e2d
amtorcasio <torcasioandreamichele@gmail.com>
2017-04-28 13:33:38 +1000

    Add project files.

---
+++ SIT321 Assignment 3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SIT321_Assignment_3.MvcApplication" Language="C#" %>
---
+++ SIT321 Assignment 3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SIT321_Assignment_3.Startup))]
+namespace SIT321_Assignment_3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
