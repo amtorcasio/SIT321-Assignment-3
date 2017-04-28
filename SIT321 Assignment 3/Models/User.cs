fb3b2962696b48b486360c66622659504d4d0749
gyurikol <gyuri.kol@outlook.com>
2017-04-27 21:22:44 -0700

    user_model

---
+++ SIT321 Assignment 3/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SIT321_Assignment_3.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Password { get; set;  }
+        public string Email { get; set; }
+    }
+}
