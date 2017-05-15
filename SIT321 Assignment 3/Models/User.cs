af64799fd85bf0e15e3b38fbc24e7036e7fa79cc
gyurikol <gyuri.kol@outlook.com>
2017-05-14 19:11:23 -0700

    removed type from user model

--- SIT321 Assignment 3/Models/User.cs
+++ SIT321 Assignment 3/Models/User.cs
@@ -10,7 +10,6 @@ namespace SIT321_Assignment_3.Models
         public int Id { get; set; }
         public string Firstname { get; set; }
         public string Lastname { get; set; }
-        public int Type { get; set; }
         public string Password { get; set; }
         public string Email { get; set; }
     }
